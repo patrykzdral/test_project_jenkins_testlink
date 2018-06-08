e7cedc0820dbab1070b74675c96eec9a84ebf322
kcieslik226138 <kcieslik226138@e-science.pl>
2018-06-08 16:11:06 +0200

    Addins script.

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-virtualenv venv --distribute
+virtualenv venv --distribute -p python3
 source venv/bin/activate
-pip install googlemaps
-PYTHONPATH=. python3 tests/calculations_tests.py
+pip install -r requirements.txt
+PYTHONPATH=. venv/bin/python tests/calculations_tests.py

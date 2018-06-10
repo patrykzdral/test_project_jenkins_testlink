3bb659e8677c553832e75927ed8c3b858ac2c880
kcieslik226138 <kcieslik226138@e-science.pl>
2018-06-10 19:19:55 +0200

    Adding coverage plugin.

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,7 @@ set -e
 virtualenv venv --distribute -p python3
 source venv/bin/activate
 pip install -r requirements.txt
+PYTHONPATH=. venv/bin/python3 tests/calculations_tests.py
 PYTHONPATH=. venv/bin/coverage-3.6 run tests/calculations_tests.py
 PYTHONPATH=. venv/bin/coverage-3.6 report
 PYTHONPATH=. venv/bin/coverage-3.6 html
